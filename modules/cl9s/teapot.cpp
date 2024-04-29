a36e87ec3de8e4eb8ef80e80b30125ce604eff8f
HanNglePC <wooyeophan@gmail.com>
2024-04-29 11:45:39 +0900

    파일 분리, 중복되는 함수 찾아서 일단 delete 함

---
+++ modules/cl9s/teapot.cpp
@@ -0,0 +1,105 @@
+#include "teapot.h"
+
+namespace cl9s
+{
+    teapot::teapot(
+        const bool& prevent_socket_creation,
+        const int& reuse_address) : reuse_address(reuse_address)
+    {
+        request_method_from_string = {
+            {"GET", request_method::GET},
+            {"HEAD", request_method::HEAD},
+            {"POST", request_method::POST},
+            {"PUT", request_method::PUT},
+            {"DELETE", request_method::DELETE},
+            {"CONNECT", request_method::CONNECT},
+            {"OPTIONS", request_method::OPTIONS},
+            {"TRACE", request_method::TRACE},
+            {"PATCH", request_method::PATCH}};
+    }
+
+    teapot::~teapot() 
+    {
+    }
+
+// static public member functions
+    const int teapot::send(
+        const sock& client_socket,
+        const char* response,
+        const size_t& response_size)
+    {
+        if (write(client_socket, response, response_size) < 0) {
+            perror("teapot_server::send() > write");
+            return EXIT_FAILURE;
+        }
+
+        return EXIT_SUCCESS;
+    }
+
+    const int teapot::receive(
+        const sock& client_socket,
+        char* buffer,
+        const size_t& buffer_size)
+    {
+        ssize_t received;
+
+        do {
+            received = read(client_socket, buffer, buffer_size);
+        } while (received > 0 && buffer[received] > 0);
+
+        if (received < 0) {
+            perror("teapot_server::receive() > read");
+            return EXIT_FAILURE;
+        }
+
+        return EXIT_SUCCESS;
+    }
+
+// public member functions
+    const int teapot::close_socket(sock& socket IN, const int& how) {
+        if (socket == 0) {
+            return EXIT_SUCCESS;
+        }
+
+        if (shutdown(socket, how) < 0) {
+            perror("teapot::close_socket() > shutdown");
+            return EXIT_FAILURE;
+        }
+
+        socket = 0;
+
+        return EXIT_SUCCESS;
+    }
+
+    const request_method& teapot::str_to_request_method(char* method) {
+        return request_method_from_string[method];
+    }
+
+// protected member functions
+    const int teapot::create_socket(
+        sock* listening_socket IN OUT,
+        const int& domain,
+        const int& type,
+        const int& protocol) const
+    {
+        *listening_socket = socket(domain, type, protocol);
+        if (*listening_socket < 0) {
+            perror("teapot::create_socket() > socket");
+            return EXIT_FAILURE;
+        }
+
+        if (reuse_address == 1) {
+            if (setsockopt(
+                    *listening_socket,
+                    SOL_SOCKET,
+                    SO_REUSEADDR,
+                    &reuse_address,
+                    sizeof(int)) < 0) {
+                perror("teapot::create_socket() > setsockopt");
+                return EXIT_FAILURE;
+            }
+        }
+
+        return EXIT_SUCCESS;
+    }
+}
--- modules/cl9s/teapot.h
+++ modules/cl9s/teapot.h
@@ -34,71 +34,25 @@ namespace cl9s
     class teapot
     {
     public:
-        teapot(
-            const bool& prevent_socket_creation = false,
-            const int& reuse_address = 1) : reuse_address(reuse_address)
-        {
+        teapot(const bool& prevent_socket_creation = false, const int& reuse_address = 1);
+        virtual ~teapot();
 
-            request_method_from_string = {
-                {"GET", request_method::GET},
-                {"HEAD", request_method::HEAD},
-                {"POST", request_method::POST},
-                {"PUT", request_method::PUT},
-                {"DELETE", request_method::DELETE},
-                {"CONNECT", request_method::CONNECT},
-                {"OPTIONS", request_method::OPTIONS},
-                {"TRACE", request_method::TRACE},
-                {"PATCH", request_method::PATCH}
-            };
-        }
-
-        virtual ~teapot() {
-        }
-    
     public:
-        // Summary:
-        //  send data to client
-        //
-        // Returns:
-        //  EXIT_SUCCESS on Success
-        //  EXIT_FAILURE on Fail
+        /// @brief Sends data to client
+        /// @param client_socket socket to send data to
+        /// @return EXIT_SUCCESS on Success, EXIT_FAILURE on Fail
         static const int send(
             const sock& client_socket,
             const char* response,
-            const size_t& response_size)
-        {
-            if (write(client_socket, response, response_size) < 0) {
-                perror("teapot_server::send() > write");
-                return EXIT_FAILURE;
-            }
+            const size_t& response_size);
 
-            return EXIT_SUCCESS;
-        }
-
-        // Summary:
-        //  receives data from client
-        //
-        // Returns:
-        //  EXIT_SUCCESS on Success
-        //  EXIT_FAILIRE on Fail
+        /// @brief Receives data from client
+        /// @param client_socket socket to receive data from
+        /// @return EXIT_SUCCESS on Success, EXIT_FAILURE on Fail
         static const int receive(
             const sock& client_socket,
             char* buffer,
-            const size_t& buffer_size)
-        {
-            ssize_t received;
-
-            do {
-                received = read(client_socket, buffer, buffer_size);
-            } while (received > 0 && buffer[received] > 0);
-
-            if (received < 0) {
-                perror("teapot_server::receive() > read");
-                return EXIT_FAILURE;
-            }
-
-            return EXIT_SUCCESS;
-        }
+            const size_t& buffer_size);
 
         // Summary:
         //  closes connection with client
@@ -108,78 +62,30 @@ namespace cl9s
         //  EXIT_FAILURE on Fail
         static const int close_connection(
             const sock& client_socket,
-            const int& how = SHUT_RDWR)
-        {
-            if (shutdown(client_socket, how)) {
-                perror("teapot_server::close_connection() > shutdown");
-                return EXIT_FAILURE;
-            }
-
-            return EXIT_SUCCESS;
-        }
+            const int& how = SHUT_RDWR) = delete; // FIXME: Duplicate function. use teapot::close_socket
 
     public:
-        // Summary:
-        //  Closes socket.
-        //
-        // Returns:
-        //  EXIT_SUCCESS on Success
-        //  EXIT_FAILURE on Fail
-        const int close_socket(sock& socket IN, const int& how = SHUT_RDWR) {
-            if (socket == 0) {
-                return EXIT_SUCCESS;
-            }
+        /// @brief Closes socket.
+        /// @param socket socket to close
+        /// @return EXIT_SUCCESS on Success, EXIT_FAILURE on Fail
+        const int close_socket(sock& socket IN, const int& how = SHUT_RDWR);
 
-            if (shutdown(socket, how) < 0) {
-                perror("teapot::close_socket() > shutdown");
-                return EXIT_FAILURE;
-            }
-
-            socket = 0;
-
-            return EXIT_SUCCESS;
-        }
-
-        const request_method& str_to_request_method(char* method) {
-            return request_method_from_string[method];
-        }
+        /// @brief Changes GET, POST, etc to request_method::GET, request_method::POST, etc.
+        /// @param method request method
+        /// @return method as request_method enum class
+        const request_method& str_to_request_method(char* method);
 
         virtual void stop(const int& how, const char* errmsg = "") = 0;
 
     protected:
-        // Summary:
-        //  Creates socket.
-        //
-        // Returns:
-        //  EXIT_SUCCESS on Success
-        //  EXIT_FAILURE on Fail
+        /// @brief Creates socket.
+        /// @param listening_socket new socket object
+        /// @return EXIT_SUCCESS on Success, EXIT_FAILURE on Fail
         const int create_socket(
             sock* listening_socket IN OUT,
             const int& domain = PF_INET,
             const int& type = SOCK_STREAM,
-            const int& protocol = IPPROTO_TCP
-        ) const {
-            *listening_socket = socket(domain, type, protocol);
-            if (*listening_socket < 0) {
-                perror("teapot::create_socket() > socket");
-                return EXIT_FAILURE;
-            }
-
-            if (reuse_address == 1) {
-                if (setsockopt(
-                        *listening_socket,
-                        SOL_SOCKET,
-                        SO_REUSEADDR,
-                        &reuse_address,
-                        sizeof(int)) < 0)
-                {
-                    perror("teapot::create_socket() > setsockopt");
-                    return EXIT_FAILURE;
-                }
-            }
-
-            return EXIT_SUCCESS;
-        }
+            const int& protocol = IPPROTO_TCP) const;
 
     private:
         std::unordered_map<const char*, request_method> request_method_from_string;
