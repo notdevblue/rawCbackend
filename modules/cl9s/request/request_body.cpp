055e924369b80211dd29a36790beb5f0f49cea42
HanNglePC <wooyeophan@gmail.com>
2024-05-28 18:51:37 +0900

    working on multipart body parsing

--- modules/cl9s/request/request_body.cpp
+++ modules/cl9s/request/request_body.cpp
@@ -11,10 +11,6 @@ namespace cl9s
     void parse(const std::string&, const std::string&);
 
     void request::parse_form_data(const std::string& body, const std::string& boundary) {
-        // split with boundary
-        // content-disposition: form-data; name="asfdasdf"; ...
-        // name="asdasdf" -> m_body[name] = data;
-
         const std::size_t current_boundary_index = body.find(boundary);
         if (current_boundary_index == std::string::npos) {
             return;
@@ -29,6 +25,12 @@ namespace cl9s
         const std::string content = temp_content_str.substr(0, next_boundary_index - 3);
 
         std::cout << content << std::endl;
+
+        // content-disposition: form-data; name="asfdasdf"; ...
+        // name="asdasdf" -> m_body[name] = data;
+
+        const std::size_t name_index = content.find("name="); // get name -> name="<this>"\n
+        const std::size_t subcontent_begin_index = content.find("\n\n"); // get content -> \n\n<content>
 
         parse_form_data(temp_content_str, boundary);
         return;
